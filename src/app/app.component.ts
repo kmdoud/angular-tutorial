d239cc8a0fdb82d016936669a1cc1311362f2c7a
Kenneth M Doud <kmdoud@gmail.com>
2019-03-13 16:32:54 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.html', //points to html
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-hello-world';
+  title: string = 'Angular 7';
+  subtitle: string = 'From .NET bootcamp 5';
+
+  inputTxt:string = "default input text";
+
+  changebutton(txt:string): void 
+  {
+    this.title = txt;
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, MenuComponent],
+  imports:      [BrowserModule],
+  providers:    [],
+  bootstrap:    [AppComponent]
 })
 export class AppModule { }
